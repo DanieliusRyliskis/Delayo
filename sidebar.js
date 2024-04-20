568354c9fd9a5e1722a6188adafdbf80af9ff15b
Danielius Ryliskis <ryliskisdanielius@gmail.com>
2024-04-20 18:02:29 +0300

    Sidebar CSS fix
    Fixes the sidebar CSS creating grid__wraper that contains display:grid instead of having both position and display properties on main nav. Also adds menuToggleOff function to sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -3,10 +3,15 @@ const menuOpen = document.getElementById("menu--open");
 const menuClose = document.getElementById("menu--close");
 const sidebar = document.querySelector("main nav");
 
-const menuToggle = function (e) {
+const menuToggleOn = function (e) {
   e.preventDefault();
   sidebar.style.display = "initial";
-  menuOpen.style.visibility = "hidden";
+};
+const menuToggleOff = function (e) {
+  e.preventDefault();
+  sidebar.style.display = "none";
 };
 
-menuOpen.addEventListener("click", menuToggle);
+menuOpen.addEventListener("click", menuToggleOn);
+
+menuClose.addEventListener("click", menuToggleOff);
