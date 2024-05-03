736e54a1cd961c504567e71b4e38834b76b65126
Danielius Ryliskis <ryliskisdanielius@gmail.com>
2024-05-03 16:56:50 +0300

    Made active link animation
    Adds active link that shows on what page you're currently in. Also fixes the size of sidebar on different screen widths

--- sidebar.js
+++ sidebar.js
@@ -4,8 +4,40 @@ const menuClose = document.getElementById("menu--close");
 const sidebar = document.querySelector("main nav");
 const sidebarActive = document.querySelector(".sidebar--active");
 const overlay = document.querySelector(".overlay");
+// querySelectorAll returns a static not live nodelist
 const menuItem = document.querySelectorAll(".menu__item");
 const menuItemSvg = document.querySelectorAll(".menu__item svg");
+const currentPage = document.title;
+
+const itemColorChange = function (element, elementColor) {
+  element.style.color = elementColor;
+  const svg = element.firstElementChild;
+  const paths = svg.children;
+  for (let item of paths) {
+    if (item.classList.contains("menu__stroke")) {
+      item.style.stroke = elementColor;
+    } else if (item.classList.contains("menu__fill")) {
+      item.style.fill = elementColor;
+    }
+  }
+};
+
+menuItem.forEach((menuItem) => {
+  menuItem.classList.remove("menu--active");
+});
+if (currentPage === "Home page") {
+  const currentItem = document.getElementById("Home");
+  currentItem.classList.add("menu--active");
+  itemColorChange(currentItem, "#f9f366");
+} else if (currentPage === "Tasks") {
+  const currentItem = document.getElementById("Tasks");
+  currentItem.classList.add("menu--active");
+  itemColorChange(currentItem, "#f9f366");
+} else if (currentPage === "Settings") {
+  const currentItem = document.getElementById("Tasks");
+  currentItem.classList.add("menu--active");
+  itemColorChange(currentItem, "#f9f366");
+}
 
 // Slide on and off
 const menuToggleOn = function (e) {
@@ -23,38 +55,30 @@ menuOpen.addEventListener("click", menuToggleOn);
 
 menuClose.addEventListener("click", menuToggleOff);
 
-const itemColorChange = function (element, elementColor) {
-  element.style.color = elementColor;
-  const svg = element.firstElementChild;
-  const paths = svg.children;
-  for (let item of paths) {
-    if (item.classList.contains("menu__stroke")) {
-      item.style.stroke = elementColor;
-    } else if (item.classList.contains("menu__fill")) {
-      item.style.fill = elementColor;
-    }
-  }
-};
-
 // Menu hover
-menuItem.forEach((e) =>
-  e.addEventListener("mouseenter", function (event) {
-    console.log(event.target);
-    // debugger;
-    const hoveredElement = event.target;
-    itemColorChange(hoveredElement, "#f9f366");
-    // Change the siblings
-    menuItem.forEach((sibling) => {
-      if (sibling !== hoveredElement) {
-        itemColorChange(sibling, "black");
-      }
+menuItem.forEach((e) => {
+  if (!e.classList.contains("menu--active")) {
+    e.addEventListener("mouseenter", function (event) {
+      const hoveredElement = event.target;
+      itemColorChange(hoveredElement, "#f9f366");
+      // Change the siblings
+      menuItem.forEach((sibling) => {
+        if (
+          sibling !== hoveredElement &&
+          !sibling.classList.contains("menu--active")
+        ) {
+          itemColorChange(sibling, "black");
+        }
+      });
     });
-  })
-);
+  }
+});
 
-menuItem.forEach((e) =>
-  e.addEventListener("mouseleave", function (event) {
-    const hoveredElement = event.target;
-    itemColorChange(hoveredElement, "black");
-  })
-);
+menuItem.forEach((e) => {
+  if (!e.classList.contains("menu--active")) {
+    e.addEventListener("mouseleave", function (event) {
+      const hoveredElement = event.target;
+      itemColorChange(hoveredElement, "black");
+    });
+  }
+});
